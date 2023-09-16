05264a0e6c8732c88f8899acc73a5b82fedfa4d3
highmania <117874127+highmania@users.noreply.github.com>
2023-09-16 11:03:13 +0900

    Add files via upload

--- script/app.js
+++ script/app.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
   //스크롤 이벤트
   $(document).on("scroll", function() {
     let scrollTop = $(window).scrollTop();
-      if (scrollTop > 100) {
+      if (scrollTop > 50) {
         $("header, .submenu_bg").addClass("active");
       } else {
         $("header, .submenu_bg").removeClass("active");
@@ -104,7 +104,21 @@ $(document).ready(function() {
       }
     ]
   });
-
+  // Top버튼 보이기 | 숨기기 
+  $(window).scroll(function() { 
+      if ($(this).scrollTop() > 250) { //250 넘으면 버튼이 보여짐니다. 
+          $('#toTop').fadeIn(); 
+          $('#toTop').css('left', $('#sidebar').offset().left); // #sidebar left:0 죄표 
+      } else { 
+          $('#toTop').fadeOut(); 
+      } 
+  }); 
+  // 버튼 클릭시 
+  $("#toTop").click(function() { 
+      $('html, body').animate({ scrollTop : 0 // 0 까지 animation 이동합니다. 
+      }, 400); // 속도 400 return false; 
+  }); 
+  
   //gsap 세팅
   gsap.utils.toArray(".show").forEach((item) => {
     ScrollTrigger.create({
