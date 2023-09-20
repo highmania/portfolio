2835e96f02053a3e193ab2a4cce8785f423c3335
highmania <117874127+highmania@users.noreply.github.com>
2023-09-20 09:54:18 +0900

    Add files via upload

--- script/app.js
+++ script/app.js
@@ -104,19 +104,19 @@ $(document).ready(function() {
       }
     ]
   });
-  // Top버튼 보이기 | 숨기기 
+  // Top버튼
   $(window).scroll(function() { 
-      if ($(this).scrollTop() > 250) { //250 넘으면 버튼이 보여짐니다. 
+      if ($(this).scrollTop() > 250) { //250 넘으면 버튼활성화 
           $('#toTop').fadeIn(); 
-          $('#toTop').css('left', $('#sidebar').offset().left); // #sidebar left:0 죄표 
+          $('#toTop').css('left', $('#sidebar').offset());
       } else { 
           $('#toTop').fadeOut(); 
       } 
   }); 
-  // 버튼 클릭시 
+  // Top 버튼 클릭시 
   $("#toTop").click(function() { 
-      $('html, body').animate({ scrollTop : 0 // 0 까지 animation 이동합니다. 
-      }, 400); // 속도 400 return false; 
+      $('html, body').animate({ scrollTop : 0 // 0 까지 animation 이동 
+      }, 400); // 리턴 속도  
   }); 
   
   //gsap 세팅
